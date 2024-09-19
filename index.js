bc73e23087e5f113b010e15e3ebbc420a6d59538
maxwel kirimi <maxwelkirimii@gmail.com>
2024-09-19 20:55:57 +0300

    document structure

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(Ralph) {
+    cats.push (Ralph);
+}
+function destructivelyRemoveLastCat(Garfield){
+    cats.pop (Garfield);
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift (name);
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift ();
+}
+function appendCat(name){
+    return [...cats, name];
+}
+function prependCat(name){
+    return [name, ...cats];
+}
+function removeLastCat() {
+    return cats.slice(0, cats.length -1)
+}
+function removeFirstCat(){
+    return cats.slice(1);
+}
+
